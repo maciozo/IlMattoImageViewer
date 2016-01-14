a6934fc0a328fa4f55fbf53432d6213dbd341e59
maciozo <maciek1663@gmail.com>
2016-01-14 22:47:25 +0000

    Made some changes to mode2 - still not working

--- bmp2matto.py
+++ bmp2matto.py
@@ -178,56 +178,48 @@ int main()
 {
     uint16_t x, y, column, row = 0, colour = 0;
     uint16_t r, g, b;
+    uint8_t p;
     rectangle pixel;
-    uint8_t pixelData[%d][%d] PROGMEM = !BMP_DATA!;
+    uint8_t pixelData[60][40] PROGMEM = !BMP_DATA!;
     init_lcd();
-    for (x = 0; x < 240; x += %d)
+    for (x = 0; x < 240; x += 4)
     {
         column = 0;
-        for (y = 0; y < 320; y += %d)
+        for (y = 0; y < 320; y += 4)
         {
-            pixel.left = x;
-            pixel.right = x + %d;
-            pixel.top = y;
-            pixel.bottom = y + %d;
-            r = 0;
-            g = 0; 
-            b = 0;
-            
-            //RGB121 -> RGB565
-            colour = pixelData[row][column] >> 4;
-            r = ((colour & 0b1000) >> 3) << 15;
-            g = ((colour & 0b0110) >> 1) << 9;
-            b = (colour & 0b0001) << 4;
-            colour = (r | g | b);
-            
-            fill_rectangle(pixel, colour);
+            for (p = 0; p <= 1; p++)
+            {
+                pixel.left = x;
+                pixel.right = x + 4;
+                pixel.top = y;
+                pixel.bottom = y + 4;
+                r = 0;
+                g = 0;
+                b = 0;
+                
+                //RGB121 -> RGB565
+                if (p == 0)
+                {
+                    r = (pixelData[column][row] & 0b10000000) << 8;
+                    g = (pixelData[column][row] & 0b01100000) << 3;
+                    b = (pixelData[column][row] & 0b00010000);
+                } 
+                else
+                {
+                    r = (pixelData[column][row] & 0b00001000) << 12;
+                    g = (pixelData[column][row] & 0b00000110) << 8;
+                    b = (pixelData[column][row] & 0b00000001) << 4;
+                }
+                colour = (r | g | b);
+                fill_rectangle(pixel, colour);
+                y += 4;
+            }
             column++;
-            y++;
-            
-            pixel.left = x;
-            pixel.right = x + %d;
-            pixel.top = y;
-            pixel.bottom = y + %d;
-            r = 0;
-            g = 0; 
-            b = 0;
-            
-            //RGB121 -> RGB565
-            colour = pixelData[row][column] & 0b00001111;
-            r = ((colour & 0b1000) >> 3) << 15;
-            g = ((colour & 0b0110) >> 1) << 9;
-            b = (colour & 0b0001) << 4;
-            colour = (r | g | b);
-            
-            fill_rectangle(pixel, colour);
-            column++;
-            
         }
         row++;
     }
 }
-""" % (width/2, height/2, x, y, x, y, x, y)
+"""
 
 
 
@@ -319,27 +311,43 @@ def mode1g():
 def mode2(): # 2 Pixels per uint_8t
     pixel = 0;
     pixel1 = 0b11110000; #4 bits per pixel in 8 bit integer
-    # print(pixels)
+    squishedGrid = [];
+    for i in range(0, 60):
+        new = []
+        for j in range(0, 40):
+            new.append(0)
+        squishedGrid.append(new)
     for row in range(0, height - 1):
         for col in range(0, width):
             rgb = pixels[pixel]
             
             #RGB888 -> RGB121
-            red = (int(rgb[0]) >> 7) << 7
-            green = (int(rgb[1]) >> 6) << 1
-            blue = (int(rgb[2]) >> 7)
-            rgb = (red | green | blue)
+            r = (int(rgb[0]) & 0b10000000) >> 4
+            g = (int(rgb[1]) & 0b11000000) >> 5
+            b = (int(rgb[2]) & 0b10000000) >> 7
+            rgb = (r | g | b)
             
-            if (col % 2 == 0):
-                pixel1 = rgb << 4 #First of 2 pixels, move to front of uint_8t
-            else:
-                pixel = pixel1 | rgb #Last of 2 pixels, place in last 4 bits of uint_8t
-                pixelGrid[col][row / 2] = hex(pixel)
+            pixelGrid[col][row] = hex(rgb)
+            pixel += 1
+                
+    colnum = 0
+    print(len(pixelGrid))
+    for col in pixelGrid:
+        print(colnum)
+        rownum = 0
+        for row in pixelGrid:
+            if (rownum % 2 == 0):
+                #print(rownum)
+                squishedGrid[colnum][int(rownum / 2)] = hex((int(pixelGrid[colnum][rownum], 16) << 4) | int(pixelGrid[colnum][rownum + 1], 16))
+            rownum += 1
+        colnum += 1
             
-for item in pixelGrid:
-        open("output.c", "w")
-        with open("output.c", "a") as file:
-            file.write(mode2str.replace("!BMP_DATA!", str(pixelGrid).replace("[", "{").replace("]", "}").replace("'", "")))
+    for col in squishedGrid:
+        print(col)
+    for item in squishedGrid:
+            open("output.c", "w")
+            with open("output.c", "a") as file:
+                file.write(mode2str.replace("!BMP_DATA!", str(squishedGrid).replace("[", "{").replace("]", "}").replace("'", "")))
             
 if (mode == 0 and not greyscale):
     mode0()
